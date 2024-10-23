c2f90e8cd11ba847ff3418eace2f0bd7640e11dd
soufianesylas-cmd <53211817+soufianesylas-cmd@users.noreply.github.com>
2024-10-23 13:39:37 -0700

    Update test.java

--- test.java
+++ test.java
@@ -1,26 +1,9 @@
-import java.util.Scanner;
+public class Test {
 
-public class Addition {
+	public static void main(String[] args) {
+		System.out.println("**************************************");
+		System.out.println("Hello from GITHub");
+		System.out.println("**************************************");
+	}
 
-    public static void main(String[] args) {
-        // Create a Scanner object for input
-        Scanner scanner = new Scanner(System.in);
-
-        // Ask the user for the first number
-        System.out.print("Enter the first number: ");
-        int num1 = scanner.nextInt();
-
-        // Ask the user for the second number
-        System.out.print("Enter the second number: ");
-        int num2 = scanner.nextInt();
-
-        // Perform the addition
-        int sum = num1 + num2;
-
-        // Output the result
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
-
-        // Close the scanner
-        scanner.close();
-    }
 }
