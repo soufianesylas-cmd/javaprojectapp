b0cc5762621765b0b0b26cd06506937aaeb5f1a2
soufianesylas-cmd <53211817+soufianesylas-cmd@users.noreply.github.com>
2024-10-29 07:05:32 -0700

    Add files via upload

---
+++ App.test.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testAddition() {
+        App app = new App();
+        int result = app.addition(10, 5);
+        assertEquals(15, result);
+    }
+}
